bf82b3b6fc5f0b006dbaf837074cd0083ae6ade7
HectorCast <49396252+HectorCast@users.noreply.github.com>
2019-04-08 20:43:58 -0500

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-066663db63b3aa675"
+  ami           = "ami-0481727d0e48e4da7"
   instance_type = "t2.micro"
   key_name = "terraform-key"
   security_groups = ["${aws_security_group.allow_rdp.name}"]
